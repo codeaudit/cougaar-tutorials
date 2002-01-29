21e12597918572aafb8baee0ac82368360e4577b
krotherm <krotherm>
2002-01-29 21:44:54 +0000

    Part of fix for bug 850

--- tutorial/bin/makejar.bat
+++ tutorial/bin/makejar.bat
@@ -23,4 +23,5 @@ REM $id$
 rem
 cd ..\src
 jar cvf ..\lib\tutorial.jar org\cougaar\tutorial\faststart\*.class org\cougaar\tutorial\faststart\hanoi\*.class org\cougaar\tutorial\faststart\binary\*.class org\cougaar\tutorial\faststart\computer\*.class org\cougaar\tutorial\faststart\calendar\*.class org\cougaar\tutorial\faststart\computer\assets\*.class
-cd ..\bin
+copy ..\lib\tutorial.jar %COUGAAR_INSTALL_PATH%\SYS\tutorial.jar
+cd ..\configs
