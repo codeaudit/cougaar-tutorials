94ecba13b46c410f92e63de26b7addf634b7e42d
krotherm <krotherm>
2002-01-29 21:16:44 +0000

    Fix for bug 850 Also added ability to use PlanViewServlet instead of Tasks.PSP

--- tutorial/bin/Run.bat
+++ tutorial/bin/Run.bat
@@ -19,22 +19,20 @@ REM " TORTIOUS CONDUCT, ARISING OUT OF OR IN CONNECTION WITH THE USE OR"
 REM " PERFORMANCE OF THE COUGAAR SOFTWARE."
 REM "</copyright>"
 
-REM $id$
 
 if "%COUGAAR_INSTALL_PATH%"=="" goto AIP_ERROR
 if "%1"=="" goto ARG_ERROR
 
 set LIBPATHS=%COUGAAR_INSTALL_PATH%\lib\core.jar
-set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\xml4j_2_0_11.jar
-set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\planserver.jar
-set LIBPATHS=..\lib\tutorial.jar;%LIBPATHS%
 
 REM pass in "NodeName" to run a specific named Node
 
-set MYPROPERTIES=
+set MYPROPERTIES= -Dorg.cougaar.system.path=%COUGAAR_INSTALL_PATH%\sys -Dorg.cougaar.install.path=%COUGAAR_INSTALL_PATH% -Dorg.cougaar.core.servlet.enable=true -Dorg.cougaar.lib.web.scanRange=100 -Dorg.cougaar.lib.web.http.port=8800 -Dorg.cougaar.lib.web.https.port=-1 -Dorg.cougaar.lib.web.https.clientAuth=true
+
 set MYMEMORY=
 set MYCLASSES=org.cougaar.core.node.Node
 set MYARGUMENTS= -c -n "%1"
+
 
 @ECHO ON
 
@@ -46,7 +44,7 @@ echo Please set COUGAAR_INSTALL_PATH
 goto QUIT
 
 :ARG_ERROR
-echo Run requires an argument  eg: Run HanoiNode
+echo Run requires an argument  eg: Run ExerciseOneNode
 goto QUIT
 
 :QUIT
