973e1cc18d0c75bcb2d53723a7cbe6d74857934c
krotherm <krotherm>
2002-01-30 22:50:26 +0000

    Updated build script and run script to work with version 9.0

--- tutorial/doc/exercises/Exercise1/build.bat
+++ tutorial/doc/exercises/Exercise1/build.bat
@@ -22,9 +22,13 @@ rem Script to compile tutorial project
 
 echo on
 
+rem Regenerate and recompile all property/asset files
+call makeassets
+
 rem compile the code
 set LIBPATHS=%COUGAAR_INSTALL_PATH%\lib\core.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\build.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\glm.jar
 set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\lib\planserver.jar
-javac -classpath %LIBPATHS% tutorial\*.java
+set LIBPATHS=%LIBPATHS%;%COUGAAR_INSTALL_PATH%\sys\servlet.jar
+javac -d bin -classpath %LIBPATHS% tutorial\*.java && jar cf %COUGAAR_INSTALL_PATH%\sys\course.jar -C bin ./tutorial
