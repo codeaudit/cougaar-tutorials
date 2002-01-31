2b7a4087795de2e665e46bffd768f9825e96e0e1
krotherm <krotherm>
2002-01-31 20:08:20 +0000

    fixed the location of jar file

--- tutorial/bin/makejar.bat
+++ tutorial/bin/makejar.bat
@@ -23,5 +23,5 @@ REM $id$
 rem
 cd ..\src
 jar cvf ..\lib\tutorial.jar org\cougaar\tutorial\faststart\*.class org\cougaar\tutorial\faststart\hanoi\*.class org\cougaar\tutorial\faststart\binary\*.class org\cougaar\tutorial\faststart\computer\*.class org\cougaar\tutorial\faststart\calendar\*.class org\cougaar\tutorial\faststart\computer\assets\*.class
-copy ..\lib\tutorial.jar %COUGAAR_INSTALL_PATH%\SYS\tutorial.jar
+copy ..\lib\tutorial.jar %COUGAAR_INSTALL_PATH%\lib\tutorial.jar
 cd ..\configs
