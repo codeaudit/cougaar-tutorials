46814177cb45b199ef0894b25df76358c0520842
jwong <jwong>
2004-01-21 15:33:34 +0000

    Fixed deprecated getVerb call

--- tutorial/src/org/cougaar/tutorial/exercise3/ManagerAllocatorPlugin.java
+++ tutorial/src/org/cougaar/tutorial/exercise3/ManagerAllocatorPlugin.java
@@ -2,11 +2,11 @@
  * <copyright>
  *  Copyright 1997-2003 BBNT Solutions, LLC
  *  under sponsorship of the Defense Advanced Research Projects Agency (DARPA).
- * 
+ *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the Cougaar Open Source License as published by
  *  DARPA on the Cougaar Open Source Website (www.cougaar.org).
- * 
+ *
  *  THE COUGAAR SOFTWARE AND ANY DERIVATIVE SUPPLIED BY LICENSOR IS
  *  PROVIDED 'AS IS' WITHOUT WARRANTIES OF ANY KIND, WHETHER EXPRESS OR
  *  IMPLIED, INCLUDING (BUT NOT LIMITED TO) ALL IMPLIED WARRANTIES OF
@@ -43,7 +43,7 @@ class myTaskPredicate implements UnaryPredicate{
     boolean ret = false;
     if (o instanceof Task) {
       Task t = (Task)o;
-      ret = t.getVerb().equals(Verb.getVerb("CODE"));
+      ret = t.getVerb().equals(Verb.get("CODE"));
     }
     return ret;
   }
@@ -62,7 +62,7 @@ class myProgrammersPredicate implements UnaryPredicate{
  * This COUGAAR Plugin allocates tasks of verb "CODE"
  * to ProgrammerAssets
  * @author ALPINE (alpine-software@bbn.com)
- * @version $Id: ManagerAllocatorPlugin.java,v 1.2 2003-12-15 17:36:02 tom Exp $
+ * @version $Id: ManagerAllocatorPlugin.java,v 1.3 2004-01-21 15:33:34 jwong Exp $
  **/
 public class ManagerAllocatorPlugin extends ComponentPlugin {
 
