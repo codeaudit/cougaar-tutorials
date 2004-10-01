fb4f47ea01e7f76aa8e3b2d36598ff837316ecc5
lgoldsto <lgoldsto>
2004-10-01 14:40:45 +0000

    String constants class for the pizza party app.

---
+++ pizza/src/org/cougaar/pizza/Constants.java
@@ -0,0 +1,11 @@
+package org.cougaar.pizza;
+
+/**
+ *  String constants for the pizza party application
+ */
+public class Constants {
+  // Private constructor prevents instantiation
+  private Constants() {}
+
+  public static final String ORDER = "ORDER";
+}
