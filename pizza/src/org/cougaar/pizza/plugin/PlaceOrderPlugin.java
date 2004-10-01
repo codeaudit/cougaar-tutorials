70143ddbec4ccaf5c78c41ccf259063a7483b76c
lgoldsto <lgoldsto>
2004-10-01 15:59:04 +0000

    Added more methods, some predicates and loaded some services.

--- pizza/src/org/cougaar/pizza/plugin/PlaceOrderPlugin.java
+++ pizza/src/org/cougaar/pizza/plugin/PlaceOrderPlugin.java
@@ -26,13 +26,22 @@
 package org.cougaar.pizza.plugin;
 
 import org.cougaar.core.plugin.ComponentPlugin;
+import org.cougaar.core.service.DomainService;
 import org.cougaar.core.service.LoggingService;
+import org.cougaar.pizza.Constants;
+import org.cougaar.planning.ldm.PlanningFactory;
+import org.cougaar.planning.ldm.plan.Allocation;
+import org.cougaar.planning.ldm.plan.NewTask;
+import org.cougaar.planning.ldm.plan.Task;
+import org.cougaar.planning.ldm.plan.Verb;
+import org.cougaar.util.UnaryPredicate;
 
 /**
- *
+ * Plugin for ordering pizzas -- more documentation later
  */
 public class PlaceOrderPlugin extends ComponentPlugin {
   private LoggingService logger;
+  private DomainService domainService;
 
   public void load() {
     super.load();
@@ -44,8 +53,66 @@ public class PlaceOrderPlugin extends ComponentPlugin {
   }
 
   protected void setupSubscriptions() {
+    domainService = getDomainService();
+  }
+  /**
+   * Used by the binding utility through reflection to set my DomainService
+   */
+  public void setDomainService(DomainService aDomainService) {
+    domainService = aDomainService;
+  }
+
+  /**
+   * Used by the binding utility through reflection to get my DomainService
+   */
+  public DomainService getDomainService() {
+    return domainService;
   }
 
   protected void execute() {
+  }
+
+  private PlanningFactory getPlanningFactory() {
+    PlanningFactory factory = null;
+    if (domainService != null) {
+      factory = (PlanningFactory) domainService.getFactory("planning");
+    }
+    return factory;
+  }
+
+  // TODO:  placeholder for now, may need to change significantly
+  private Task makeTask() {
+    PlanningFactory planningFactory = getPlanningFactory();
+    NewTask newTask = planningFactory.newTask();
+    newTask.setVerb(Verb.get(Constants.ORDER));
+    newTask.setPlan(planningFactory.getRealityPlan());
+    // TODO: update to real pizza asset
+    newTask.setDirectObject(planningFactory.createAsset("Pizza"));
+    return newTask;
+  }
+
+  /**
+   * A predicate that filters for InviteList objects
+   */
+  static class TaskPredicate implements UnaryPredicate{
+    public boolean execute(Object o) {
+      //return (o instanceof InviteList);
+      return false;
+    }
+
+    /**
+     * A predicate that filters for allocations of "ORDER" tasks
+     */
+    static class AllocationPredicate implements UnaryPredicate{
+      public boolean execute(Object o) {
+        if (o instanceof Allocation) {
+          Task t = ((Allocation)o).getTask();
+          if (t != null) {
+            return t.getVerb().equals(Verb.get(Constants.ORDER));
+          }
+        }
+        return false;
+      }
+    }
   }
 }
