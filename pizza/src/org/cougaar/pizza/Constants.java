cd854b81a3d3fd2bddb0d3a65c31b9cb8122b1d9
gvidaver <gvidaver>
2004-10-02 00:15:24 +0000

    added invitiation query and expected num friends

--- pizza/src/org/cougaar/pizza/Constants.java
+++ pizza/src/org/cougaar/pizza/Constants.java
@@ -8,6 +8,11 @@ public class Constants {
   private Constants() {}
 
   public static final String ORDER = "ORDER";
+
+  /** the query to my friends */
+  public static final String INVITATION_QUERY = "invitation-meat_or_veg";
+  public static final int EXPECTED_NUM_FRIENDS = 4;
+
   public static final String FIND_PROVIDERS = "FindProviders";
   public static final String PIZZA_PROVIDER = "PizzaProvider";
 }
