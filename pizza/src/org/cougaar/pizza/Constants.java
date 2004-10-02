005e578061edb8668aa638f4de2f1dfe596aa688
gvidaver <gvidaver>
2004-10-02 02:07:16 +0000

    added Carnivore, Vegetarian roles

--- pizza/src/org/cougaar/pizza/Constants.java
+++ pizza/src/org/cougaar/pizza/Constants.java
@@ -11,8 +11,12 @@ public class Constants {
 
   /** the query to my friends */
   public static final String INVITATION_QUERY = "invitation-meat_or_veg";
+
+  /* used when showing how long it takes for this number of friends to reply */
   public static final int EXPECTED_NUM_FRIENDS = 4;
 
   public static final String FIND_PROVIDERS = "FindProviders";
   public static final String PIZZA_PROVIDER = "PizzaProvider";
+  public static final String CARNIVORE = "Carnivore";
+  public static final String VEGETARIAN = "Vegitarian";
 }
