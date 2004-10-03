f991746395fdbb4fcf8490bca867c69cbecd98f3
bdepass <bdepass>
2004-10-03 23:43:21 +0000

    Add code to set the ItemID PG on the pizza assets so they have unique names. The servlet/bboard seems to be confused without that even though these assets have unique ids. Also add code to fill in the new PG slots that cause the PGs to print in the Pizza Asset's xml

--- pizza/src/org/cougaar/pizza/plugin/PizzaPrototypePlugin.java
+++ pizza/src/org/cougaar/pizza/plugin/PizzaPrototypePlugin.java
@@ -90,35 +90,49 @@ public class PizzaPrototypePlugin extends ComponentPlugin {
 
     // Create a Veggie Pizza Asset based on the existing pizza prototype
     PizzaAsset veggie_pizza_asset = (PizzaAsset) factory.createInstance("pizza");
-    veggie_pizza_asset.setVeggiePG(makeAVeggiePG());  // Veggie pizza
+    veggie_pizza_asset.setVeggiePG(makeAVeggiePG(true));  // Veggie pizza
+    veggie_pizza_asset.setItemIdentificationPG(makeAItemIdentificationPG("Veggie Pizza"));
     getBlackboardService().publishAdd(veggie_pizza_asset);
+//    System.out.println("publishAdding veggie pizza asset: "+ veggie_pizza_asset +
+//        " uid is: " + veggie_pizza_asset.getUID());
 
     // Create a Meat Pizza Asset based on the existing pizza prototype
     PizzaAsset meat_pizza_asset = (PizzaAsset) factory.createInstance("pizza");
-    meat_pizza_asset.setMeatPG(makeAMeatPG());   // Meat pizza
+    meat_pizza_asset.setMeatPG(makeAMeatPG(true));   // Meat pizza
+    meat_pizza_asset.setItemIdentificationPG(makeAItemIdentificationPG("Meat Pizza"));
     getBlackboardService().publishAdd(meat_pizza_asset);
-
+//    System.out.println("publishAdding meat pizza asset: " + meat_pizza_asset +
+//        " uid is: " + meat_pizza_asset.getUID());
   }
 
   /**
    * Create and populate a Veggie property group
    */
-  private VeggiePG makeAVeggiePG() {
+  private VeggiePG makeAVeggiePG(boolean veggieOnly) {
     NewVeggiePG new_veggie_pg = (NewVeggiePG)
       ((PlanningFactory)getDomainService().getFactory("planning")).createPropertyGroup("VeggiePG");
-    //TODO: Doe we want to put something inside the PG??
-    //new_skills_pg.setYearsExperience(yearsExperience);
+    new_veggie_pg.setVeggieOnly(veggieOnly);
     return new_veggie_pg;
   }
 
   /**
    * Create and populate a Meat property group
    */
-  private MeatPG makeAMeatPG() {
+  private MeatPG makeAMeatPG(boolean meatOnly) {
     NewMeatPG new_meat_pg = (NewMeatPG)
       ((PlanningFactory)getDomainService().getFactory("planning")).createPropertyGroup("MeatPG");
-    //new_skills_pg.setYearsExperience(yearsExperience);
+    new_meat_pg.setMeatOnly(meatOnly);
     return new_meat_pg;
+  }
+
+  /**
+   * Create and populate an ItemIdentification property group
+   */
+  private ItemIdentificationPG makeAItemIdentificationPG(String name) {
+    NewItemIdentificationPG new_item_id_pg = (NewItemIdentificationPG)
+      ((PlanningFactory)getDomainService().getFactory("planning")).createPropertyGroup("ItemIdentificationPG");
+    new_item_id_pg.setItemIdentification(name);
+    return new_item_id_pg;
   }
 
   /**
