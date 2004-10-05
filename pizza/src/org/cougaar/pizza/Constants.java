156dbc9f5a3762e0919603f8ba3c467371fbdbbc
lgoldsto <lgoldsto>
2004-10-05 20:40:04 +0000

    Fixed typo in Vegitarian and added PIZZA.

--- pizza/src/org/cougaar/pizza/Constants.java
+++ pizza/src/org/cougaar/pizza/Constants.java
@@ -18,5 +18,6 @@ public class Constants {
   public static final String FIND_PROVIDERS = "FindProviders";
   public static final String PIZZA_PROVIDER = "PizzaProvider";
   public static final String CARNIVORE = "Carnivore";
-  public static final String VEGETARIAN = "Vegitarian";
+  public static final String VEGETARIAN = "Vegetarian";
+  public static final String PIZZA = "Pizza";
 }
