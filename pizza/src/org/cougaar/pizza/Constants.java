5c4361871413d69a06cae1e49b095e8b5203982d
lgoldsto <lgoldsto>
2004-10-19 13:41:55 +0000

    Put back in the constant PIZZA_PROVIDER to fix the build.  Lowercased the Order string.

--- pizza/src/org/cougaar/pizza/Constants.java
+++ pizza/src/org/cougaar/pizza/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
   // Private constructor prevents instantiation
   private Constants() {}
 
-  public static final String ORDER = "ORDER";
+  public static final String ORDER = "Order";
 
   /** the query to my friends */
   public static final String INVITATION_QUERY = "invitation-meat_or_veg";
@@ -16,7 +16,7 @@ public class Constants {
   public static final int EXPECTED_NUM_FRIENDS = 4;
 
   public static final String FIND_PROVIDERS = "FindProviders";
-  //public static final String PIZZA_PROVIDER = "PizzaProvider";
+  public static final String PIZZA_PROVIDER = "PizzaProvider";
   public static final String CARNIVORE = "Carnivore";
   public static final String VEGETARIAN = "Vegetarian";
   public static final String PIZZA = "Pizza";
