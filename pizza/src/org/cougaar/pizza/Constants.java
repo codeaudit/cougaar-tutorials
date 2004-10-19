9e3f97fd70c78d8c6fe44732f3d38165e94fc6d4
ngivler <ngivler>
2004-10-19 18:14:15 +0000

    Removed PIZZA_PROVIDER - removed all references.

--- pizza/src/org/cougaar/pizza/Constants.java
+++ pizza/src/org/cougaar/pizza/Constants.java
@@ -16,7 +16,6 @@ public class Constants {
   public static final int EXPECTED_NUM_FRIENDS = 4;
 
   public static final String FIND_PROVIDERS = "FindProviders";
-  public static final String PIZZA_PROVIDER = "PizzaProvider";
   public static final String CARNIVORE = "Carnivore";
   public static final String VEGETARIAN = "Vegetarian";
   public static final String PIZZA = "Pizza";
