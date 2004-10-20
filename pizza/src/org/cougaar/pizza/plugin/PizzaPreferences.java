8337673423f8b09f24e48c84d611fc5d459fce4d
lgoldsto <lgoldsto>
2004-10-20 21:02:09 +0000

    Reformatted code and optimized the imports.

--- pizza/src/org/cougaar/pizza/plugin/PizzaPreferences.java
+++ pizza/src/org/cougaar/pizza/plugin/PizzaPreferences.java
@@ -26,58 +26,76 @@
 
 package org.cougaar.pizza.plugin;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
-import org.cougaar.core.mts.MessageAddress;
 import org.cougaar.core.service.LoggingService;
 import org.cougaar.core.util.UID;
 import org.cougaar.core.util.UniqueObject;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Stores mapping from friend to their pizza preference
- *
+ * <p/>
  * Counts meat and veg preferences.
  */
 
 public class PizzaPreferences implements UniqueObject {
   protected UID uid;
   protected Map friendToPizza = new HashMap();
-  private   LoggingService log;
+  private LoggingService log;
   protected int numMeat = 0;
-  protected int numVeg  = 0;
+  protected int numVeg = 0;
 
   // for UniqueObject interface
-  public UID getUID() { return uid; }
-  public void setUID(UID uid) { this.uid = uid; }
+  public UID getUID() {
+    return uid;
+  }
 
-  public void addFriendToPizza (String friend, String preference) {
+  public void setUID(UID uid) {
+    this.uid = uid;
+  }
+
+  public void addFriendToPizza(String friend, String preference) {
     friendToPizza.put(friend, preference);
 
-    if (preference.toLowerCase().equals ("meat"))
+    if (preference.toLowerCase().equals("meat"))
       numMeat++;
-    else if (preference.toLowerCase().equals ("veg"))
+    else if (preference.toLowerCase().equals("veg"))
       numVeg++;
     else
-      log.warn ("unknown preference " + preference + " for " + friend);
+      log.warn("unknown preference " + preference + " for " + friend);
   }
 
-  public String getPreferenceForFriend (String friend) {
-    return (String) friendToPizza.get (friend);
+  public String getPreferenceForFriend(String friend) {
+    return (String) friendToPizza.get(friend);
   }
 
-  public Set getFriends () { return friendToPizza.keySet(); }
+  public Set getFriends() {
+    return friendToPizza.keySet();
+  }
 
-  public String getFriendNames () { return friendToPizza.keySet().toString(); }
-  public String getPreferenceValues () { return friendToPizza.values().toString(); }
-  public String getFriendToPreference () { return friendToPizza.toString(); }
+  public String getFriendNames() {
+    return friendToPizza.keySet().toString();
+  }
 
-  public int getNumMeat() { return numMeat; }
-  public int getNumVeg () { return numVeg; }
+  public String getPreferenceValues() {
+    return friendToPizza.values().toString();
+  }
 
-  public String toString () {
+  public String getFriendToPreference() {
+    return friendToPizza.toString();
+  }
+
+  public int getNumMeat() {
+    return numMeat;
+  }
+
+  public int getNumVeg() {
+    return numVeg;
+  }
+
+  public String toString() {
     return "Party guests pizza preferences : " + friendToPizza;
   }
 }
