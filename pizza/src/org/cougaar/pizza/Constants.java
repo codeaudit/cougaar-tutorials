64c400ab2ecedf5fe1ccd1e42af93e8621b44de1
lgoldsto <lgoldsto>
2004-10-28 14:47:06 +0000

    Reformatted.

--- pizza/src/org/cougaar/pizza/Constants.java
+++ pizza/src/org/cougaar/pizza/Constants.java
@@ -3,18 +3,21 @@ package org.cougaar.pizza;
 import java.io.File;
 
 /**
- *  String constants for the pizza party application
+ * String constants for the pizza party application
  */
 public class Constants {
   // Private constructor prevents instantiation
-  private Constants() {}
+  private Constants() {
+  }
 
   public static final String ORDER = "Order";
 
-  /** the query to my friends */
+  /**
+   * the query to my friends
+   */
   public static final String INVITATION_QUERY = "invitation-meat_or_veg";
   public static final String INVITATION_REPLY_MEAT = "meat";
-  public static final String INVITATION_REPLY_VEG  = "veg";
+  public static final String INVITATION_REPLY_VEG = "veg";
 
   /* used when showing how long it takes for this number of friends to reply */
   public static final int EXPECTED_NUM_FRIENDS = 4;
@@ -25,23 +28,21 @@ public class Constants {
   public static final String PIZZA = "Pizza";
 
   public interface Preposition {
-    String NOT         =  "Not";
+    String NOT = "Not";
   }
 
   public interface RelationshipType {
     String PROVIDER_SUFFIX = "Provider";
     String CUSTOMER_SUFFIX = "Customer";
-    org.cougaar.planning.ldm.plan.RelationshipType PROVIDER = 
-      org.cougaar.planning.ldm.plan.RelationshipType.create(PROVIDER_SUFFIX, CUSTOMER_SUFFIX);
+    org.cougaar.planning.ldm.plan.RelationshipType PROVIDER =
+        org.cougaar.planning.ldm.plan.RelationshipType.create(PROVIDER_SUFFIX, CUSTOMER_SUFFIX);
   }
 
   public static class Role {
     /**
-     * Insure that Role constants are initialized. Actually does
-     * nothing, but the classloader insures that all static
-     * initializers have been run before executing any code in this
-     * class.
-     **/
+     * Insure that Role constants are initialized. Actually does nothing, but the classloader insures that all static
+     * initializers have been run before executing any code in this class.
+     */
     public static void init() {
     }
 
@@ -51,24 +52,19 @@ public class Constants {
     }
 
     // organization roles
-    public static final org.cougaar.planning.ldm.plan.Role SELF = 
-      org.cougaar.planning.ldm.plan.Role.getRole("Self");
+    public static final org.cougaar.planning.ldm.plan.Role SELF = org.cougaar.planning.ldm.plan.Role.getRole("Self");
 
-    public static final org.cougaar.planning.ldm.plan.Role PIZZAPROVIDER = 
-      org.cougaar.planning.ldm.plan.Role.getRole("Pizza" + 
-                                RelationshipType.PROVIDER_SUFFIX);
-    public static final org.cougaar.planning.ldm.plan.Role PIZZACUSTOMER = 
-      org.cougaar.planning.ldm.plan.Role.getRole("PIZZA" + 
-                                RelationshipType.CUSTOMER_SUFFIX);
+    public static final org.cougaar.planning.ldm.plan.Role PIZZAPROVIDER =
+        org.cougaar.planning.ldm.plan.Role.getRole("Pizza" + RelationshipType.PROVIDER_SUFFIX);
+    public static final org.cougaar.planning.ldm.plan.Role PIZZACUSTOMER =
+        org.cougaar.planning.ldm.plan.Role.getRole("PIZZA" + RelationshipType.CUSTOMER_SUFFIX);
   }
 
   public static String getDataPath() {
-    return System.getProperty("org.cougaar.install.path") + File.separator +
-      "pizza" + File.separator + "data";
+    return System.getProperty("org.cougaar.install.path") + File.separator + "pizza" + File.separator + "data";
   }
 
   public interface UDDIConstants {
-
     public final static String COMMERCIAL_SERVICE_SCHEME = "CommercialServiceScheme";
     public final static String COMMERCIAL_SERVICE_SCHEME_UUID = "uuid:f0b01564-b8f0-b015-dad5-b49598339719";
     public final static String ORGANIZATION_TYPES = "OrganizationTypes";
