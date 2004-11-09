1042844e875c4c8649f0a0f8d535a29e9511120f
ngivler <ngivler>
2004-11-09 19:00:22 +0000

    Additional documentation.

--- pizza/src/org/cougaar/pizza/util/RoleScorer.java
+++ pizza/src/org/cougaar/pizza/util/RoleScorer.java
@@ -38,6 +38,19 @@ import org.cougaar.servicediscovery.description.ServiceInfoScorer;
 import org.cougaar.util.log.Logger;
 import org.cougaar.util.log.Logging;
 
+/* Uses 2 criteria -  
+ *   service role must match scorer role
+ *   service provider name must not match one of the names on the blacklist.
+ *
+ * All passing descriptions get a score of 1, all failing descriptions get
+ * a score of -1.
+ * 
+ * SDClientPlugin creates the RoleScorer and attaches it to the MMQueryRequest.
+ * MatchmakerPlugin uses the RoleScorer to evaluate service descriptions
+ * returned from the yellow pages. All passing service descriptions are added
+ * to the MMQueryRequest results field.
+ * 
+ */
 public class RoleScorer implements ServiceInfoScorer {
   private static Logger logger = Logging.getLogger(RoleScorer.class);
   Role myRole;
