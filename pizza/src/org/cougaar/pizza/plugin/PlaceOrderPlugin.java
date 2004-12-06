f2547ea046d286b45f0fe93e0f3cfd27a770b05f
ahelsing <ahelsing>
2004-12-06 22:16:03 +0000

    javadoc logging cleanup put the logging of the expansion results in a generic protected method that actually looks at the subtasks / direct object / etc Only create a meat / veggie subtask if any were requested use isLocal and not isSelf

--- pizza/src/org/cougaar/pizza/plugin/PlaceOrderPlugin.java
+++ pizza/src/org/cougaar/pizza/plugin/PlaceOrderPlugin.java
@@ -55,21 +55,22 @@ import org.cougaar.util.UnaryPredicate;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.Iterator;
 import java.util.Vector;
 
 /**
- * This plugin orders pizza for a pizza party. It subscribes to a PizzaPreferences object
- * that is published by the InvitePlugin.  The PizzaPreferences object contains the types
- * of pizzas to order and the number of individuals requesting each pizza type.  After the
- * plugin receives a PizzaPreferences object it creates and publishes a Task with the Verb
- * "Order" and a direct object of type Pizza (Asset).  Next, it expands the order task
+ * This plugin orders the pizza for a pizza party. It subscribes to a {@link PizzaPreferences} object
+ * published by the {@link InvitePlugin} when it knows how much of what kind of pizza to order.
+ * <p>
+ * On receiving the PizzaPreferences, it creates and publishes a Task with the Verb
+ * "Order" and a Direct Object of type Pizza (Asset).  Next, it expands the Order Task
  * into a workflow of two subtasks, one subtask per type of pizza with a quantity
- * Preference for the number of requests.  To place the order, it allocates the substasks
+ * Preference for the number of requests.  To place the order, it allocates the subtasks
  * to its pizza provider, Joes Local Pizza Shack.  This customer/provider relationship is
  * defined in the XML configuration files and is established when the agents start up. See
  * PizzaNode1.xml and PizzaNode2.xml for details.
- *
+ * <p>
  * In this example, the plugin cannot successfully complete the pizza order because Joe's
  * Local Pizza Shack doesn't make veggie pizzas.  The final result is a failed Expansion
  * on the parent order Task due to the failed Allocation of the veggie pizza subtask.
@@ -118,7 +119,8 @@ public class PlaceOrderPlugin extends ComponentPlugin {
   }
 
   /**
-   * Initialize the subcriptions the plugin is interested in.
+   * Initialize the subcriptions the plugin is interested in: self Entity, the PizzaPreferences,
+   * and existing Allocations or Expansions or Order Tasks.
    */
   protected void setupSubscriptions() {
     selfSub = (IncrementalSubscription) getBlackboardService().subscribe(SELF_PRED);
@@ -128,10 +130,15 @@ public class PlaceOrderPlugin extends ComponentPlugin {
   }
 
   /**
-   * Execute is called when there are changes to the plugins's subscriptions.
+   * When there are changes to the plugins's subscriptions, we:
+   * 1) When the PizzaPreferences arrives, create the root Order Task, and expand
+   * it by the types of pizza. Then allocate these to the available provider.
+   * 2) Propagate up any changed Allocation Results
+   * 3) See if the root Expansion changed. If so, we may be done.
    */
   protected void execute() {
-    // The PizzaPreferences object contains the party invitation responses.
+    // The PizzaPreferences object contains the party invitation responses,
+    // and indicates we should start
     PizzaPreferences pizzaPrefs = getPizzaPreferences();
     if ( pizzaPrefs != null) {
       // Create the parent order task.
@@ -144,21 +151,54 @@ public class PlaceOrderPlugin extends ComponentPlugin {
       allocateSubtasks(pizzaSubtasks, getProvider());
     }
 
-    // Update changes to the results of the allocation.
+    // Update changes to the results of the allocation - propagate the changes up
     updateOrderAllocationResults();
-    // Check for changes on the Expansion
+
+    // Check for changes on the Expansion - we may be done
     Expansion exp = getChangedExpansion();
     if (exp != null) {
       if (logger.isDebugEnabled()) {
         logger.debug(" Change received on the expansion " + printExpansionResults(exp));
       }
-      // If the Expansion was not successful print party order results
+
+      // If the Expansion is confident, then we are done. Print the results.
+      // Note that just because we're done, doesn't mean we have our pizza
       if (exp.getReportedResult().getConfidenceRating() == 1.0) {
-	// FIXME: Print what we actually did, not all constants
-	// 
-	logger.shout("Pizza Order Task FAILed: Joe's couldn't handle Order for for 2 Veggie and 2 Meat pizzas.");
-      }
-    }
+	logExpansionResults(exp);
+      } // Expansion is confident
+    } // have an updated expansion
+  }
+
+  /**
+   * Log (at SHOUT) the results of the given Expansion, showing
+   * what was ordered, how much, from whom, and with what success.
+   * @param exp The Expansion of the root Order task, whose details we print
+   */
+  protected void logExpansionResults(Expansion exp) {
+    Task t = exp.getTask();
+    Verb v = t.getVerb();
+    boolean succ = exp.getReportedResult().isSuccess();
+
+    // The sub-tasks (1 per pizza type)
+    Enumeration subs = exp.getWorkflow().getTasks();
+
+    logger.shout("Pizza " + v + " Task " + (succ ? "SUCCEEDED " : "FAILED "));
+
+    // Log details of each sub-task
+    while (subs.hasMoreElements()) {
+      Task sub = (Task)subs.nextElement();
+
+      // Get quantity, type of pizza ordered
+      double qty = sub.getPreferredValue(AspectType.QUANTITY);
+      String piztype = sub.getDirectObject().getItemIdentificationPG().getItemIdentification();
+
+      // Get the store we ordered from
+      String store = ((Allocation)sub.getPlanElement()).getAsset().getItemIdentificationPG().getItemIdentification();
+
+      // Remember that each sub-task succeeds or fails independently
+      boolean subSucc = sub.getPlanElement().getReportedResult().isSuccess();
+      logger.shout("     " + store + " could" + (subSucc ? "" : " NOT") + " handle " + v + " for " + qty + " " + piztype);
+    } // loop over sub-tasks
   }
 
   /**
@@ -166,7 +206,7 @@ public class PlaceOrderPlugin extends ComponentPlugin {
    * added collection on the subscription and returns the first element in the
    * collection.  Will return null if the added collection is empty.
    *
-   * @return a PizzaPrefereneces object from the subscription
+   * @return first PizzaPrefereneces object from the subscription
    */
   protected PizzaPreferences getPizzaPreferences() {
     for (Iterator i = pizzaPrefSub.getAddedCollection().iterator(); i.hasNext();) {
@@ -205,27 +245,35 @@ public class PlaceOrderPlugin extends ComponentPlugin {
    */
   protected Collection createPizzaSubtasks(PizzaPreferences pizzaPrefs, Task parentTask) {
     ArrayList subtasks = new ArrayList(2);
-    // Create the meat pizza subtask
-    PizzaAsset meatPizza = makePizzaAsset(Constants.MEAT_PIZZA);
-    // Add meat properties to the pizza
-    meatPizza.addOtherPropertyGroup(PropertyGroupFactory.newMeatPG());
-    NewTask meatPizzaTask = makeTask(Constants.Verbs.ORDER, meatPizza);
-    // Make a quantity Preference to represent the number of requests for meat pizza
-    Preference meatPref = makeQuantityPreference(pizzaPrefs.getNumMeat());
-    meatPizzaTask.setPreference(meatPref);
-    meatPizzaTask.setParentTask(parentTask);
-    subtasks.add(meatPizzaTask);
 
-    // Create the veggie pizza subtask
-    PizzaAsset veggiePizza = makePizzaAsset(Constants.VEGGIE_PIZZA);
-    // Add veggie properties to the pizza
-    veggiePizza.addOtherPropertyGroup(PropertyGroupFactory.newVeggiePG());
-    NewTask veggiePizzaTask = makeTask(Constants.Verbs.ORDER, veggiePizza);
-    // Make a quantity Preference to represent the number of requests for veggie pizza
-    Preference veggiePref = makeQuantityPreference(pizzaPrefs.getNumVeg());
-    veggiePizzaTask.setPreference(veggiePref);
-    veggiePizzaTask.setParentTask(parentTask);
-    subtasks.add(veggiePizzaTask);
+    if (pizzaPrefs.getNumMeat() > 0) {
+      // Create the meat pizza subtask
+      PizzaAsset meatPizza = makePizzaAsset(Constants.MEAT_PIZZA);
+      // Add meat properties to the pizza
+      // Note the use of our Pizza-domain PropertyGroupFactory, which 
+      // has the custom newFooPG methods. 
+      meatPizza.addOtherPropertyGroup(PropertyGroupFactory.newMeatPG());
+      NewTask meatPizzaTask = makeTask(Constants.Verbs.ORDER, meatPizza);
+      // Make a quantity Preference to represent the number of requests for meat pizza
+      Preference meatPref = makeQuantityPreference(pizzaPrefs.getNumMeat());
+      meatPizzaTask.setPreference(meatPref);
+      meatPizzaTask.setParentTask(parentTask);
+      subtasks.add(meatPizzaTask);
+    }
+
+    if (pizzaPrefs.getNumVeg() > 0) {
+      // Create the veggie pizza subtask
+      PizzaAsset veggiePizza = makePizzaAsset(Constants.VEGGIE_PIZZA);
+      // Add veggie properties to the pizza
+      veggiePizza.addOtherPropertyGroup(PropertyGroupFactory.newVeggiePG());
+      NewTask veggiePizzaTask = makeTask(Constants.Verbs.ORDER, veggiePizza);
+      // Make a quantity Preference to represent the number of requests for veggie pizza
+      Preference veggiePref = makeQuantityPreference(pizzaPrefs.getNumVeg());
+      veggiePizzaTask.setPreference(veggiePref);
+      veggiePizzaTask.setParentTask(parentTask);
+      subtasks.add(veggiePizzaTask);
+    }
+
     return subtasks;
   }
 
@@ -276,8 +324,16 @@ public class PlaceOrderPlugin extends ComponentPlugin {
     }
     for (Iterator iter = subtasks.iterator(); iter.hasNext();) {
       Task task = (Task) iter.next();
+      // create Result for the given task, using the planningFactory, with
+      // a confidence of 0.25, estimate success=true
       AllocationResult ar = PluginHelper.createEstimatedAllocationResult(task, planningFactory,
                                                                          0.25, true);
+      // Create an allocation for the given task & plan, allocating the task to 
+      // the given asset - an Entity asset, which will cause the task
+      // to be copied to the Agent that represents that Entity.
+      // Give the allocation the initial Estimated result, and indicate
+      // that the Entity is performing the PizzaProvider role for you
+      // in handling this Task
       Allocation alloc = planningFactory.createAllocation(task.getPlan(), task, (Asset) provider,
                                                           ar, Constants.Roles.PIZZAPROVIDER);
       getBlackboardService().publishAdd(alloc);
@@ -288,12 +344,18 @@ public class PlaceOrderPlugin extends ComponentPlugin {
   }
 
   /**
-   * Updates the estimated AllocationResults with reported AllocationResults. Checks the
+   * Updates the estimated AllocationResults with reported AllocationResults. 
+   * <p> 
+   * Remember that the infrastructure sets the ReportedResult for you (in this case,
+   * by copying it from the provider agent when the provider settles the Task), but
+   * you must copy that up typically.
+   * <p>
+   * Checks the
    * changed collection on the Allocation subscription.  If the collection is not empty,
    * it retrieves the Allocation PlanElement from the subscription.  If the reported
    * AllocationResults have changed, the results are udpated and a publish change is
    * called on the PlanElement.  The reported AllocationResults are changed by the
-   * provider assinged to the task.
+   * provider assigned to the task.
    */
   protected void updateOrderAllocationResults() {
     for (Iterator i = allocationSub.getChangedCollection().iterator(); i.hasNext();) {
@@ -314,11 +376,13 @@ public class PlaceOrderPlugin extends ComponentPlugin {
   }
 
   /**
-   * Returns the Expansion that was changed.  Checks the changed collection on the
+   * Returns the Expansion that was changed (the one and only typically).  
+   * <p>
+   * Checks the changed collection on the
    * Subscription.  If the collection is not empty, it returns the first element. If empty
    * it returns null.
    *
-   * @return the Expansion that was changed
+   * @return the Expansion that was changed if any
    */
   protected Expansion getChangedExpansion() {
     for (Iterator i = expansionSub.getChangedCollection().iterator(); i.hasNext();) {
@@ -331,27 +395,30 @@ public class PlaceOrderPlugin extends ComponentPlugin {
   /**
    * Returns a NewTask.  Creates a task with the specified verb and sets the Asset as the
    * direct object.
-   * @param verb the verb of the task
-   * @param directObject the asset
+   * @param verb the verb of the newtask
+   * @param directObject asset that this Task is acting on
    * @return a NewTask
    */
   protected NewTask makeTask(Verb verb, Asset directObject) {
     NewTask newTask = planningFactory.newTask();
     newTask.setVerb(verb);
-    newTask.setPlan(planningFactory.getRealityPlan());
+    newTask.setPlan(planningFactory.getRealityPlan()); // Plan is bogus - always use Reality
     newTask.setDirectObject(directObject);
     return newTask;
   }
 
   /**
-   * Returns an instance of a PizzaAsset.  Creates an instance of a PizzaAsset of the
-   * specified asset type.  Adds ItemIdentification property groups to the pizzaAsset
-   * instance.
+   * Creates an instance of a PizzaAsset of the
+   * specified asset type.  Adds an ItemIdentification property group to the pizzaAsset
+   * instance marking the pizza type.
    *
-   * @param assetType the name of the type of asset
+   * @param assetType the name of the type of asset (veg/meat)
    * @return a PizzaAsset
    */
   protected PizzaAsset makePizzaAsset(String assetType) {
+    // Note that the PizzaPrototypePlugin registered the PizzaAsset prototype with
+    // the planning factory, so now the planning factory knows
+    // how to create a pizza asset for us.
     PizzaAsset pizzaAsset = (PizzaAsset) planningFactory.createInstance(Constants.PIZZA);
     NewItemIdentificationPG itemIDPG = PropertyGroupFactory.newItemIdentificationPG();
     itemIDPG.setItemIdentification(assetType);
@@ -360,11 +427,14 @@ public class PlaceOrderPlugin extends ComponentPlugin {
   }
 
   /**
-   * Returns a quantity Preference.  Creates a Preferenece with a ScoringFunction and a
-   * quantity AspectType from the specified value.
+   * Returns a quantity Preference.  
+   * <p>
+   * Creates a Preference with a Strictly-at ScoringFunction and a
+   * quantity AspectType from the specified value. In other words,
+   * says I want this many, and you get 0 credit for any more or less.
    *
-   * @param value the value of the quantity preference
-   * @return a Preference representing a quantity
+   * @param value the number required
+   * @return a Preference requesting that number of items exactly
    */
   protected Preference makeQuantityPreference(int value) {
     ScoringFunction sf = ScoringFunction.createStrictlyAtValue(AspectValue.newAspectValue(AspectType.QUANTITY, value));
@@ -428,7 +498,7 @@ public class PlaceOrderPlugin extends ComponentPlugin {
   protected static final UnaryPredicate SELF_PRED = new UnaryPredicate() {
     public boolean execute(Object o) {
       if (o instanceof Entity) {
-        return ((Entity) o).isSelf();
+        return ((Entity) o).isLocal();
       }
       return false;
     }
