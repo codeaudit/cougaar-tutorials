73ea19620db6c58961451b9b186a6dab97c8cd41
ahelsing <ahelsing>
2004-12-09 22:22:01 +0000

    drop unused imports add javadoc, comments clean up log statements (wrapping in ifEnabled if necc) initialize warning_cutoff_time in a static get some services via reflection drop unused myOutstandingAlarms counter (only needed for quiescence) drop obsolete query handling (unused in this app) rename getNextContextFailed so it doesnt look like a method drop resetWarningCutoffTime (unused)

--- pizza/src/org/cougaar/pizza/plugin/MatchmakerPlugin.java
+++ pizza/src/org/cougaar/pizza/plugin/MatchmakerPlugin.java
@@ -29,7 +29,6 @@ package org.cougaar.pizza.plugin;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
 import java.util.Iterator;
 
 import org.cougaar.core.agent.service.alarm.Alarm;
@@ -37,7 +36,6 @@ import org.cougaar.core.blackboard.IncrementalSubscription;
 import org.cougaar.core.plugin.ComponentPlugin;
 import org.cougaar.core.service.LoggingService;
 import org.cougaar.pizza.Constants;
-import org.cougaar.planning.ldm.plan.Role;
 import org.cougaar.servicediscovery.description.MMRoleQuery;
 import org.cougaar.servicediscovery.description.ScoredServiceDescriptionImpl;
 import org.cougaar.servicediscovery.description.ServiceClassification;
@@ -48,32 +46,47 @@ import org.cougaar.servicediscovery.transaction.MMQueryRequest;
 import org.cougaar.servicediscovery.transaction.MMQueryRequestImpl;
 import org.cougaar.servicediscovery.transaction.RegistryQuery;
 import org.cougaar.servicediscovery.transaction.RegistryQueryImpl;
-import org.cougaar.util.TimeSpan;
 import org.cougaar.util.UnaryPredicate;
 
 /**
+ * The Matchmaker is responsible for taking service discovery requests (MMQueryRequests)
+ * from the SDClient, and issuing asynchronous queries to the YP
+ * to find matching providers. When one (or more) is found, send the scored
+ * results back the SDClient on the MMQueryRequest.
+ *<p>
+ * This version assumes the Role requested will be in the CommercialServiceScheme, and allows the YP
+ * to handle walking up YP communities as necessary. It does not handle quiescence,
+ * is not guaranteed to work with kills/restarts (persistence), only works
+ * with a distributed YP (using communities, not a single static instance), etc.
  *
- * Query the YellowPages for possible service providers
- *
+ * @property org.cougaar.pizza.plugin.MatchmakerQueryGracePeriod (in minutes, default is 2) specifies 
+ * how long to wait before YP query errors should be logged at ERROR instead of DEBUG.
  */
 public class MatchmakerPlugin extends ComponentPlugin {
-  private static int WARNING_SUPPRESSION_INTERVAL = 2;
-  private long myWarningCutoffTime = 0;
   private static final String QUERY_GRACE_PERIOD_PROPERTY = 
                 "org.cougaar.pizza.plugin.MatchmakerQueryGracePeriod";
+  private static final int DEFAULT_WARNING_SUPPRESSION_INTERVAL = 2; // minutes
+  private static final int WARNING_SUPPRESSION_INTERVAL; // minutes
+
+  static {
+      WARNING_SUPPRESSION_INTERVAL = 
+	Integer.getInteger(QUERY_GRACE_PERIOD_PROPERTY,
+			   DEFAULT_WARNING_SUPPRESSION_INTERVAL).intValue();
+  }
+
+  private long myWarningCutoffTime = 0;
 
   private LoggingService myLoggingService;
   private RegistryQueryService myRegistryQueryService;
   private IncrementalSubscription myClientRequestSubscription;
 
-  // outstanding RQ are those which have been issued but have not yet returned
+  /** outstanding RQ are those which have been issued but have not yet returned */
   private ArrayList myOutstandingRQs = new ArrayList();
-  // pending RQs are returned RQ which haven't been consumed by the plugin yet
+
+  /** pending RQs are returned RQ which haven't been consumed by the plugin yet */
   private ArrayList myPendingRQs = new ArrayList();
 
-  // Outstanding alarms (any means non-quiescent)
-  private int myOutstandingAlarms = 0; 
-
+  /** Subscribe to MMQueryRequests from the SDClient */
   private UnaryPredicate myQueryRequestPredicate =
     new UnaryPredicate() {
       public boolean execute(Object o) {
@@ -85,25 +98,27 @@ public class MatchmakerPlugin extends ComponentPlugin {
       }
     };
 
+  /** Reflection sets the RegistryQueryService at startup - plugin will not load if not found. */
+  public void setRegistryQueryService(RegistryQueryService rqs) {
+    myRegistryQueryService = rqs;
+  }
+
+  /**
+   * Over-ride parent load to get non-essential services (the log service here).
+   */
   public void load() {
     super.load();
 
     myLoggingService = (LoggingService)
-      getServiceBroker().getService(this, 
-				    LoggingService.class, 
-				    null);
+      getServiceBroker().getService(this, LoggingService.class, null);
     if (myLoggingService == null) {
       myLoggingService = LoggingService.NULL;
     }
-
-    myRegistryQueryService = (RegistryQueryService)
-      getServiceBroker().getService(this,
-				    RegistryQueryService.class,
-				    null);
-    if (myRegistryQueryService == null)
-      throw new RuntimeException("Unable to obtain RegistryQuery service");
   }
 
+  /**
+   * Every load() should have an unload(), to unload() the manually loaded service.
+   */
   public void unload() {
     if (myRegistryQueryService != null) {
       getServiceBroker().releaseService(this,
@@ -119,18 +134,30 @@ public class MatchmakerPlugin extends ComponentPlugin {
 					myLoggingService);
       myLoggingService = null;
     }
+
     super.unload();
   }
 
+  /**
+   * Subscribe to MMQueryRequests from the SDClientPlugin.
+   */
   protected void setupSubscriptions() {
     myClientRequestSubscription = 
       (IncrementalSubscription) getBlackboardService().subscribe(myQueryRequestPredicate);
   }
 
- 
+  /**
+   * Loop through new MMQueryRequests, posting a new asynchronous YP query for each. Also
+   * runs when the YP query callbacks signal that the plugin should run. So loop through the pending
+   * requests, posting alarms to retry those with errors later, and sending back the answer
+   * to the SDClient for those that have a list of providers.
+   *<p>
+   * Note it uses the CommercialServiceScheme as the place where the Roles will be defined in the YP, 
+   * as defined in the pizza module.
+   */
   protected void execute() {
+    // Look at all the new MMQueryRequests from the SDClientPlugin
     if (myClientRequestSubscription.hasChanged()) {
-
       for (Iterator i = myClientRequestSubscription.getAddedCollection().iterator();
 	   i.hasNext();) {
         MMQueryRequest queryRequest =  (MMQueryRequest) i.next();
@@ -138,55 +165,78 @@ public class MatchmakerPlugin extends ComponentPlugin {
         RegistryQuery rq = new RegistryQueryImpl();
 	RQ r;
 
-        // Find all service providers for specifed Role
+        // Find all service providers for specifed Role (as code and name) under the Commercial Service Scheme.
         ServiceClassification roleSC =
 	  new ServiceClassificationImpl(query.getRole().toString(),
 					query.getRole().toString(),
 					Constants.UDDIConstants.COMMERCIAL_SERVICE_SCHEME);
         rq.addServiceClassification(roleSC);
-	myLoggingService.debug("RegistryQuery = " +rq + " " +
-			       rq.getServiceClassifications());
+	if (myLoggingService.isDebugEnabled())
+	  myLoggingService.debug("RegistryQuery = " +rq + " " +
+				 rq.getServiceClassifications());
 	r = new RQ(queryRequest, query, rq);
 
+	// Put the new query in a queue, issuing an asynchronous request with a callback
         postRQ(r);
       }
-    }
+    } // end of block for new requests from the SDClientPlugin
 
-
+    // Now handle any callbacks that have come in. When they come in, they are put
+    // on the pending Queue, and then the Blackboard is signaled to run this plugin. That is 
+    // when we get here.
     RQ r;
     while ((r = getPendingRQ()) != null) {
-      if (r.exception != null) {
-	handleException(r);
-      } else {
+      if (r.exception == null) {
+	// This means it succeeded -- send the found providers back to the SDClient
 	handleResponse(r);
+      } else {
+	// There was an exception. Log and retry later (by setting an alarm)
+	handleException(r);
       }
-    }
-  }
+    } // end of loop over pending Q, which should now be empty
 
+    // If at this point we have nothing on the post queue (new queries we just sent) 
+    // nor outstanding alarms (queries that failed and we're retrying later), then
+    // we are done (until the next SDClient query come in).
+  } // end of execute()
+
+  /**
+   * Handle an exception from a YP query callback by logging something, waiting a while, and trying again.
+   * @param r YP query that had error.
+   */
   protected void handleException(RQ r) {
-    retryErrorLog(r, ": Exception querying registry for " +
+    retryErrorLog(r, ": Exception querying YP registry for " +
       r.query.getRole().toString() +
-      ", try again later.", r.exception);
+      ", will try again later.", r.exception);
     r.exception = null;
   }
 
+  /**
+   * Handle an error with a YP lookup request by logging and retrying later.
+   * @param r YP query that had error.
+   * @param message Pretty message to print to explain the error
+   */ 
   private void retryErrorLog(RQ r, String message) {
     retryErrorLog(r, message, null);
   }
 
-  // When an error occurs, but we'll be retrying later, treat it as a DEBUG
-  // at first. After a while it becomes an error.
+  /**
+   * When an error occurs, but we'll be retrying later, treat it as a DEBUG
+   * at first. After a while it becomes an error.
+   * @param r YP query that had error.
+   * @param message Pretty message to print to explain the error
+   * @param e The exception that caused the problem
+   */
   private void retryErrorLog(RQ r, String message, Throwable e) {
+    // This needs to be random to avoid them all firing at once... FIXME!!!
     int rand = (int)(Math.random()*10000) + 1000;
     QueryAlarm alarm =
       new QueryAlarm(r, getAlarmService().currentTimeMillis() + rand);
     getAlarmService().addAlarm(alarm);
-    // Alarms silently make us non-quiescent -- so keep track of when we have any
-    myOutstandingAlarms++;
 
     if (myLoggingService.isDebugEnabled()) {
-      myLoggingService.debug(":retryErrorLog - adding a QueryAlarm for r.query.getRole()" + 
-		" alarm - " + alarm);
+      myLoggingService.debug("retryErrorLog - adding a QueryAlarm for " + r.query.getRole() + 
+		", alarm: " + alarm);
     }
 
     if(System.currentTimeMillis() > getWarningCutoffTime()) {
@@ -206,19 +256,10 @@ public class MatchmakerPlugin extends ComponentPlugin {
     MMQueryRequest queryRequest = r.queryRequest;
     MMRoleQuery query = r.query;
 
-    if (query.getObsolete()) {
-      if (myLoggingService.isDebugEnabled()) {
-	myLoggingService.debug(": handleResponse - ignoring registry query result for obsolete request - " +
-			       r.query);
-      }
-      return;
-    }
-
     Collection services = r.services;
-
     
     if (myLoggingService.isDebugEnabled()) {
-      myLoggingService.debug(": handleResponse - registry query result size is : " + 
+      myLoggingService.debug("handleResponse - registry query result size is : " + 
 			     services.size() + " for query: " + 
 			     query.getRole().toString());
     }
@@ -226,6 +267,11 @@ public class MatchmakerPlugin extends ComponentPlugin {
     ArrayList scoredServiceDescriptions = new ArrayList();
     for (Iterator iter = services.iterator(); iter.hasNext(); ) {
       ServiceInfo serviceInfo = (ServiceInfo) iter.next();
+
+      // This is where the custom ServiceInfoScorer gets used. We match the service
+      // provider returned by the YP against our rules, to decide if it will do.
+      // For the pizza app, that is the RoleWithBlacklistScorer.
+      // We insist on a positive score.
       int score = query.getServiceInfoScorer().scoreServiceInfo(serviceInfo);
 
       if (score >= 0) {
@@ -239,6 +285,7 @@ public class MatchmakerPlugin extends ComponentPlugin {
 				 " Service score: " + score);
 	}
       } else {
+	// Negative score means provider didn't pass. We'll keep looking.
 	if (myLoggingService.isDebugEnabled()) {
 	  myLoggingService.debug(": execute: ignoring Provider name - " + 
 				 serviceInfo.getProviderName() +
@@ -250,7 +297,9 @@ public class MatchmakerPlugin extends ComponentPlugin {
     }
 
     if (scoredServiceDescriptions.isEmpty()) {
-      if (!r.getNextContextFailed) {
+      if (!r.nextContextFailed) {
+	// We just didn't find any yet! Re-post, so we recurse up to the next higher
+	// "context", or YP Community. (in our example, from Cambridge to MA)
 	  if (myLoggingService.isDebugEnabled()) {
 	    myLoggingService.debug(":execute - no matching provider for " + 
 				   query.getRole() +
@@ -268,10 +317,11 @@ public class MatchmakerPlugin extends ComponentPlugin {
 
       }
     } else {
+      // We have some results. Sort them by score, so the client gets the best one.
       Collections.sort(scoredServiceDescriptions);
     }
 
-
+    // Set our results on the request from the client, and publish change it to send it back
     ((MMQueryRequestImpl) queryRequest).setResult(scoredServiceDescriptions);
     ((MMQueryRequestImpl) queryRequest).setQueryCount(queryRequest.getQueryCount() + 1);
     getBlackboardService().publishChange(queryRequest);
@@ -281,11 +331,14 @@ public class MatchmakerPlugin extends ComponentPlugin {
     }
   }
 
+  /**
+   * Get the real time after which DEBUG level problems become ERROR: This is
+   * the parametrized warning cut-off interval (in minutes, default of 2), plus
+   * the time of the first error.
+   * @return time in millis
+   */
   protected long getWarningCutoffTime() {
     if (myWarningCutoffTime == 0) {
-      WARNING_SUPPRESSION_INTERVAL = 
-	Integer.getInteger(QUERY_GRACE_PERIOD_PROPERTY,
-			   WARNING_SUPPRESSION_INTERVAL).intValue();
       myWarningCutoffTime = System.currentTimeMillis() + 
 	WARNING_SUPPRESSION_INTERVAL*60000;
     }
@@ -293,10 +346,6 @@ public class MatchmakerPlugin extends ComponentPlugin {
     return myWarningCutoffTime;
   }
   
-  protected void resetWarningCutoffTime() {
-    myWarningCutoffTime = -1;
-  }
-
   private class RQ {
     MMQueryRequest queryRequest;
     MMRoleQuery query;
@@ -307,7 +356,7 @@ public class MatchmakerPlugin extends ComponentPlugin {
     boolean complete = false;
     Object previousYPContext = null;
     Object currentYPContext = null;
-    boolean getNextContextFailed = false;
+    boolean nextContextFailed = false;
 
 
     RQ(MMQueryRequest queryRequest, MMRoleQuery query, RegistryQuery rq) {
@@ -317,7 +366,7 @@ public class MatchmakerPlugin extends ComponentPlugin {
     }
   }
 
-  // issue a async request
+  /** Issue an asynchronous request to the YP, noting the outstanding query. */
   private void postRQ(final RQ r) {
     if (myLoggingService.isDebugEnabled()) {
       myLoggingService.debug(": postRQ " + r + 
@@ -330,7 +379,7 @@ public class MatchmakerPlugin extends ComponentPlugin {
     findServiceWithDistributedYP(r);
   }
 
-  // note an async response and wake the plugin
+  /** Note an asynchronous response from the YP, and wake the plugin to handle it in the plugin thread. */
   private void pendRQ(RQ r) {
     if (myLoggingService.isDebugEnabled()) {
       myLoggingService.debug(": pendRQ " + r + " (" + r.rq + ")");
@@ -346,7 +395,7 @@ public class MatchmakerPlugin extends ComponentPlugin {
     getBlackboardService().signalClientActivity();
   }
 
-  // get a pending RQ (or null) so that we can deal with it
+  /** Pop a pending RQ of the list (or null) so that we can deal with it. */
   private RQ getPendingRQ() {
     RQ r = null;
     synchronized (myPendingRQs) {
@@ -360,6 +409,11 @@ public class MatchmakerPlugin extends ComponentPlugin {
     return r;
   }
 
+  /** 
+   * This is the workhourse: ask the YP to find a service that matches the given request,
+   * and to tell us when it is done with a callback we supply. It asks the YP to 
+   * walk up the hierarchy of YP communities to find ever broader YP servers if that is necessary.
+   */
   private void findServiceWithDistributedYP(final RQ r) {
     if (myLoggingService.isDebugEnabled()) {
       myLoggingService.debug(": findServiceWithDistributedYP - " +
@@ -370,6 +424,7 @@ public class MatchmakerPlugin extends ComponentPlugin {
 						 r.rq,
 						 new RegistryQueryService.CallbackWithContext() {
       public void invoke(Object result) {
+	// Take the given services results for our query
 	r.services = (Collection) result;
 	if (myLoggingService.isDebugEnabled()) {
 	  myLoggingService.debug(": results = " + result + 
@@ -377,8 +432,9 @@ public class MatchmakerPlugin extends ComponentPlugin {
 	}
 	flush();
       }
-      
+						     
       public void handle(Exception e) {
+	// Got some sort of error trying to do the YP lookup
 	r.exception = e;
 	if (myLoggingService.isDebugEnabled()) {
 	  myLoggingService.debug(": failed during query of " +
@@ -389,25 +445,31 @@ public class MatchmakerPlugin extends ComponentPlugin {
       }
       
       public void setNextContext(Object context){
-	if (myLoggingService.isDebugEnabled()) {
+	// If one YP community / context doesn't have a matching provider,
+	// we move up to the next one. IE, when Cambridge only
+	// contains a blacklisted provider (Joes), we move up to MA.
+	if (myLoggingService.isDebugEnabled())
 	  myLoggingService.debug(": previous YPContext " +
 				 r.currentYPContext + 
 				 " current YPContext " + context);
-	}
+
 	r.previousYPContext = r.currentYPContext;
 	r.currentYPContext = context;
 	
+	// If there is no next context, then we're out of YP servers!
 	if (context == null) {
-	  r.getNextContextFailed = true;
+	  r.nextContextFailed = true;
 	}
       }
       
       private void flush() {
+	// Put the request on the pending list for handling the next time the plugin runs
 	pendRQ(r);
       }
     });
-  }
+  } // end of findServiceWithDistributedYP
 
+  /** Alarm to add a post a query to the YP after some time. */
   public class QueryAlarm implements Alarm {
     private long expiresAt;
     private boolean expired = false;
@@ -418,12 +480,13 @@ public class MatchmakerPlugin extends ComponentPlugin {
       this.rq = rq;
     }
     public long getExpirationTime() { return expiresAt; }
+    
+    /** When the alarm fires, post the query to the YP. */
     public synchronized void expire() {
       if (!expired) {
         expired = true;
 	rq.complete = false;
 	postRQ(rq);
-	--myOutstandingAlarms;
       }
     }
 
@@ -431,7 +494,6 @@ public class MatchmakerPlugin extends ComponentPlugin {
     public synchronized boolean cancel() {
       boolean was = expired;
       expired = true;
-      --myOutstandingAlarms;
       return was;
     }
     public String toString() {
@@ -440,18 +502,5 @@ public class MatchmakerPlugin extends ComponentPlugin {
 	rq.query.getRole() + " " +
         "for MatchmakerPlugin at " + getAgentIdentifier() + ">";
     }
-  }
+  } // end of QueryAlarm definition
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
