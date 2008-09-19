e59771eec9c4677dbe370cbae98a953d668f2dec
jzinky <jzinky>
2008-09-19 16:14:48 +0000

    Got imageHolder to be sent between agents, but relay is not cleaned up

--- tools/src/org/cougaar/test/knode/experiment/bette/ImageOnewayClientFacePlugin.java
+++ tools/src/org/cougaar/test/knode/experiment/bette/ImageOnewayClientFacePlugin.java
@@ -8,15 +8,13 @@ package org.cougaar.test.knode.experiment.bette;
 
 import org.cougaar.core.qos.coordinations.oneway.OnewayClientFacePlugin;
 import org.cougaar.core.qos.coordinations.oneway.OneWay.EventType;
-import org.cougaar.core.relay.SimpleRelay;
 import org.cougaar.core.util.UniqueObject;
 
 public class ImageOnewayClientFacePlugin extends OnewayClientFacePlugin {
 	public boolean match(EventType type, UniqueObject object) {
-		if (type == EventType.RECEIVE && object instanceof SimpleRelay) {
-			SimpleRelay relay = (SimpleRelay) object;
-			Object image = relay.getQuery();
-			return image instanceof ImageHolder;
+		if (type == EventType.RECEIVE && object instanceof ImageHolder) {
+		// TODO any other test on image?
+		return true;
 		}
 		return false;
 	}
