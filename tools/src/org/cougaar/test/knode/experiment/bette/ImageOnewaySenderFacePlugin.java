2c61032a9ea52665d42b85a10fbbd4f91dd5f4a9
jzinky <jzinky>
2008-09-26 14:10:29 +0000

    Only send Images that match Stream streamName

--- tools/src/org/cougaar/test/knode/experiment/bette/ImageOnewaySenderFacePlugin.java
+++ tools/src/org/cougaar/test/knode/experiment/bette/ImageOnewaySenderFacePlugin.java
@@ -11,12 +11,25 @@ import org.cougaar.core.qos.coordinations.oneway.OnewayServerFacePlugin;
 import org.cougaar.core.qos.coordinations.oneway.OneWay.EventType;
 import org.cougaar.core.util.UID;
 import org.cougaar.core.util.UniqueObject;
+import org.cougaar.util.annotations.Cougaar;
+
+
 
 public class ImageOnewaySenderFacePlugin extends OnewayServerFacePlugin {
-   public boolean match(EventType type, UniqueObject object) {
+    static final String ANY_STREAM = "AnyStream";
+    
+    @Cougaar.Arg(name = "streamName", defaultValue = ANY_STREAM, 
+                 description = "Name of the Stream. If not set to " + ANY_STREAM +
+                 " then all streams will be sent")
+     public String streamName;
+
+
+    public boolean match(EventType type, UniqueObject object) {
         if (type == EventType.SEND && object instanceof ImageHolder) {
              ImageHolder image = (ImageHolder) object;
-             return image.length() != 0;
+             String name = image.getStreamName();
+             int length = image.length();
+             return length != 0 && (name.equals(streamName) || streamName.equals(ANY_STREAM));
         }
         return false;
     }
